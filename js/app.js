bcbfe8699a8a9c77aa7c77aa5acabf2c76a7cfdf
Paulo-Henri7 <paulohenriquetav@gmail.com>
2025-01-09 19:07:22 -0400

    Reformulando o codigo

--- js/app.js
+++ js/app.js
@@ -1,51 +1,45 @@
-let resultadoGlobal = 0;
-listaVazia();
-totalvazio();
-
+let totalDeTudo = 0;
+limpar();
 function adicionar() {
-    addProduto();
+    let quantidade = document.getElementById("quantidade").value;
+    let item = document.getElementById("produto").value;
+    if (quantidade == 0) {
+        alert("ERRO não é possivel adicionar nada");
+        return;
+    }
+    let preco = parseInt(fatiar(item, "R$", 1));
+    let itemSozinho = fatiar(item, "-", 0)
+    document.getElementById("quantidade").value = "";
+    console.log(`Quantidade desejada: ${quantidade} Item desejado: ${item}`);
+    let totalProduto = calculoProduto(quantidade, preco);
+    totalDeTudo += totalProduto;
+    console.log(totalProduto);
+    console.log(`O total é :${totalDeTudo} `);
+
+    //elemetos que aparecem na tela
+    let listaDeProdutosCarrinho = document.getElementById("lista-produtos");
+    listaDeProdutosCarrinho.innerHTML = listaDeProdutosCarrinho.innerHTML + `<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}x</span> ${itemSozinho}<span class="texto-azul">R$${preco}</span>
+        </section>`;
+    let valorTotalNaTela = document.getElementById("valor-total");
+    valorTotalNaTela.innerHTML = "R$" + totalDeTudo;
+}
+
+function calculoProduto(quantidade, preco) {
+    return quantidade * preco
+}
+
+function fatiar(item, tag1, tag2) {
+    let guarda;
+    let retorno;
+    guarda = item.split(tag1)
+    return retorno = guarda[tag2]
 }
 
 function limpar() {
-    resultadoGlobal = 0;
-    totalvazio();
-    listaVazia();
-}
-
-function addProduto() {
-    let produto = document.getElementById("produto").value;
-    let nomeProduto = produto.split("-")[0];
-    let valor = parseInt(produto.split("R$")[1]);
-    let qtde = (document.getElementById("quantidade"));
-    let quantidade = qtde.value;
-    let resultado = valor * quantidade;
-    resultadoGlobal = resultadoGlobal + resultado;
-    quantidade == 0 ? alert("Erro, nenhum produto selecionado") :
-        listaProdutos(quantidade, nomeProduto, valor);
-    selecionaTotal(resultadoGlobal);
-    document.getElementById("quantidade").value = 0;
-}
-
-function listaProdutos(quantidade, nomeProduto, valor) {
-    addcarrinho = document.getElementById("lista-produtos");
-    addcarrinho.innerHTML = addcarrinho.innerHTML + (`<section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto}<span class="texto-azul">R$${valor}</span>
-        </section>`);
-}
-
-function listaVazia() {
-    addcarrinho = document.getElementById("lista-produtos");
-
-    addcarrinho.innerHTML = addcarrinho = document.getElementById("lista-produtos");
-    addcarrinho.innerHTML = ("");
-}
-
-function selecionaTotal(resultadoGlobal) {
-    textoTotal = document.querySelector("p");
-    textoTotal.innerHTML = (`Total: R$${resultadoGlobal}`);
-}
-
-function totalvazio() {
-    textoTotal = document.querySelector("p");
-    textoTotal.innerHTML = (`<span class="texto-azul" ">R$0</span>`)
+    totalDeTudo = 0;
+    let valorTotalNaTela = document.getElementById("valor-total");
+    valorTotalNaTela.innerHTML = "R$" + totalDeTudo;
+    let listaDeProdutosCarrinho = document.getElementById("lista-produtos");
+    listaDeProdutosCarrinho.innerHTML = "";
 }
